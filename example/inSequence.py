21ed91d7b11ca3962ff9c3c7d05a581a3c778878
max <max_workspace@163.com>
2018-12-07 16:30:10 +0800

    增加序列操作脚本

---
+++ example/inSequence.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+permissions = 'rw'
+print('w' in permissions)
+print('x' in permissions)
+
+subject = '$$$ Get rich now!!! $$$'
+print("$$$ G" in subject)
+
+users = ['mlh', 'foo', 'bar']
+print(input('Enter your user name:') in users)
