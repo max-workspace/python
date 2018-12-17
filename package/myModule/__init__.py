e2c0b637633015d98fe02f933e4caebd9eb3d3ae
max <max_workspace@163.com>
2018-12-17 12:11:30 +0800

    增加自建的包和模块

---
+++ package/myModule/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+from myModule import funcLib
+
+__all__=['test1', 'test2']
+
+def test1():
+    print('myModule func test1')
+
+def test2():
+    print('myModule func test2')
+
+def test3():
+    print('myModule func test3')
+
+def test4():
+    print('myModule func test4')
---
+++ package/myModule/funcLib.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+
+def funcLib1():
+    print('funcLib module funcLib1')
